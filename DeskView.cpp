fef7d59e3d7907d0e2986c2c73df24eaa193846d
Siarzhuk Zharski <imker@gmx.li>
2011-02-24 14:33:09 +0000

    - install script fixed; - SetOrigin for replicant was set 1 pixel below - in request from Diver.

--- DeskView.cpp
+++ DeskView.cpp
@@ -223,6 +223,8 @@ void DeskView::AttachedToWindow(void)
 	BPoint pt;
 	GetPreferredSize(&pt.x, &pt.y);
 	ResizeTo(pt.x + 2, fIconY); // let some place for better look
+	BPoint ptOrg = Origin();
+	SetOrigin(ptOrg.x, ptOrg.y + 1); // im request from Diver. ;-)
 }
 
 void DeskView::UpdateViewColors()
--- install.sh
+++ install.sh
@@ -3,18 +3,30 @@ MODULE_TITLE="Keymap Switcher"
 SRC_DIR=`dirname $0`
 APP_NAME=KeymapSwitcher
 ADDON_NAME=keymap_switcher
-APP_SRC=$SRC_DIR/obj.x86/$APP_NAME
-ADDON_SRC=$SRC_DIR/obj.x86/$ADDON_NAME
+OBJ_DIR=$SRC_DIR/objects.x86-gcc4-release
+APP_SRC=$OBJ_DIR/$APP_NAME
+ADDON_SRC=$OBJ_DIR/$ADDON_NAME
+ADDON_DEST=/boot/common/add-ons/input_server/filters/ 
+APP_DEST=/boot/common/bin/ 
+CATALOGS_DEST=/boot/common/data/locale/catalogs/
+APP_MIME_SIG=x-vnd.Nexus-KeymapSwitcher
 
 answer=`alert "Do you really want to install the Keymap Switcher?" "No" "Yes"`
 if [ $answer == "No" ]; then
 	exit
 fi
 
-/boot/system/bin/install $APP_SRC   /boot/common/bin/$APP_NAME
-/boot/system/bin/install $ADDON_SRC /boot/common/add-ons/input_server/filters/$ADDON_NAME
-mimeset /boot/common/bin/$APP_NAME  
-mimeset /boot/common/add-ons/input_server/filters/$ADDON_NAME
+hey input_server quit
+hey Deskbar quit
+sleep 1
 
-#ln -s /boot/system/apps/$APP_NAME "/boot/home/config/be/Desktop Applets/Keymap Switcher"
+/boot/system/bin/install $APP_SRC   $APP_DEST/$APP_NAME
+/boot/system/bin/install $ADDON_SRC $ADDON_DEST/$ADDON_NAME
+mimeset $APP_DEST/$APP_NAME  
+mimeset $ADDON_DEST/$ADDON_NAME
+
+/boot/system/Deskbar &
+/boot/system/servers/input_server &
+sleep 1
+$APP_DEST/$APP_NAME --deskbar
 
