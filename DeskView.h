210f876d9eae41ed2e073c5df73622f70f940afd
Olivier Coursière <olivier.coursiere@laposte.net>
2018-11-03 22:52:17 +0000

    When no keymaps were selected in KeymapSwitcher, clicking in the Deskbar add-on result in a crash. We now check that there is at least one keymap in the list instead of dividing by 0 before switching to the next one.

--- DeskView.h
+++ DeskView.h
@@ -42,6 +42,7 @@ public:
 
 private:
 	void ShowContextMenu(BPoint where);
+	void SwitchToNextKeyMap();
 	void ChangeKeyMap(int32);
 	void ChangeKeyMapSilent(int32, bool force = false);
 	void Init();
